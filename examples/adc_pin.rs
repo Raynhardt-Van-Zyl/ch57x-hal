6872c8c4bb281629bd7c166ae2015abd81f7dee1
Raynhardt <raynhardt.vanzyl@azoteq.com>
2026-02-16 14:43:44 +0200

    Clean up adc_pin example warnings

--- examples/adc_pin.rs
+++ examples/adc_pin.rs
@@ -1,20 +1,12 @@
 #![no_std]
 #![no_main]
 
-use core::arch::{asm, global_asm};
 use core::fmt::Write;
 use core::writeln;
 
-use embedded_hal_1::delay::DelayNs;
-use hal::adc::{adc_to_temperature_celsius, Adc};
-use hal::dma::NoDma;
-use hal::gpio::{AnyPin, Input, Level, Output, OutputDrive, Pull};
-use hal::interrupt::Interrupt;
-use hal::isp::EEPROM_BLOCK_SIZE;
-use hal::rtc::{DateTime, Rtc};
-use hal::sysctl::Config;
+use hal::adc::Adc;
+use hal::gpio::{Input, Level, Output, OutputDrive, Pull};
 use hal::uart::UartTx;
-use hal::{pac, peripherals, Peripherals};
 use {ch57x_hal as hal, panic_halt as _};
 
 #[qingke_rt::entry]
@@ -29,19 +21,19 @@ fn main() -> ! {
 
     let mut serial = UartTx::new(p.Uart1, p.PA9, Default::default()).unwrap();
 
-    let mut download_button = Input::new(p.PB22, Pull::Up);
-    let mut reset_button = Input::new(p.PB23, Pull::Up);
+    let download_button = Input::new(p.PB22, Pull::Up);
+    let reset_button = Input::new(p.PB23, Pull::Up);
 
-    serial.blocking_flush();
+    serial.blocking_flush().unwrap();
 
     writeln!(serial, "\n\n\nHello World!").unwrap();
     writeln!(serial, "Clocks: {}", hal::sysctl::clocks().hclk).unwrap();
-    writeln!(serial, "ChipID: {:02x}", hal::signature::get_chip_id());
+    writeln!(serial, "ChipID: {:02x}", hal::signature::get_chip_id()).unwrap();
 
     // ADC part
     let mut adc = Adc::new(p.ADC, hal::adc::Config::default());
 
-    let mut vbat_channel = adc.enable_vbat();
+    let _vbat_channel = adc.enable_vbat();
     //let mut temp_sensor = adc.enable_temperature();
 
     let mut pin = p.PA4;
